5bb155ce9775fbde73621b01f2ea08a0199ba6fd
LounisAbdellou <lounisabdellou.dev@gmail.com>
2025-08-24 18:17:29 +0200

    Basic capture loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use pcap::{Capture, Device};
+
 fn main() {
-    println!("Hello, world!");
+    let main_device = match Device::lookup() {
+        Ok(device) => device,
+        Err(err) => panic!("{}", err),
+    }
+    .expect("No suitable device found.");
+
+    let mut capture = Capture::from_device(main_device)
+        .unwrap()
+        .promisc(true)
+        .snaplen(5000)
+        .open()
+        .unwrap();
+
+    while let Ok(packet) = capture.next_packet() {
+        println!("received packet! {:?}", packet);
+    }
 }
